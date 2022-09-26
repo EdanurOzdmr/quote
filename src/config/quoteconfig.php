cae68ad05beaf37853122c2658f5e2122e6dd925
Edanur Özdemir <edanurozdemir23@gmail.com>
2022-09-26 11:35:32 +0300

    QuoteServiceProvider file update config data

--- src/config/quoteconfig.php
+++ src/config/quoteconfig.php
@@ -6,9 +6,6 @@ return [
         'contactus' => [
             'When people talk, listen completely. Most people never listen',
             'The way to make people trust-worthy is to trust them.',
-            'aa',
-            'bb',
-            'cc'
         ],
     ]
     ];

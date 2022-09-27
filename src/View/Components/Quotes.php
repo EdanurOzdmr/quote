16d0035050191fea09ccf06b2997cb97a4692f37
Umut Işık <umutphp@gmail.com>
2022-09-27 12:27:54 +0300

    Minor update

--- src/View/Components/Quotes.php
+++ src/View/Components/Quotes.php
@@ -7,18 +7,17 @@ use Illuminate\Support\Facades\Request;
 use Illuminate\View\Component;
 use function view;
 
-
 class Quotes extends Component
 {
+    private $API_KEY = null;
     /**
      * Create a new component instance.
      *
      * @return void
      */
-
     public function __construct()
     {
-
+        $this->API_KEY = config('quoteconfig.api_key');
     }
 
     /**
@@ -34,38 +33,43 @@ class Quotes extends Component
     public function quotes()
     {
         $pagename = Request::path();
-        $data = Collection::make(config('quoteconfig.categories'));
-        $quotes = $this->quotesAPI($data[$pagename]);
+        $data     = Collection::make(config('quoteconfig.categories'));
+        $category = isset($data[$pagename]) ? $data[$pagename] : $data['default'];
+        $quotes   = $this->quotesAPI($category);
 
-        return $quotes;
+        if ($quotes) {
+            return $quotes;
+        }
 
+        return '';
     }
 
     public function quotesAPI($category)
     {
-
         $curl = curl_init();
+
         curl_setopt_array($curl, [
             CURLOPT_URL => "https://api.api-ninjas.com/v1/quotes?category=$category",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => [
-                "X-Api-Key: h1dF2oUsjtF0pOVkH4j/Xg==Ire99vyXK87psSEh",
+            CURLOPT_ENCODING       => "",
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 30,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => "GET",
+            CURLOPT_HTTPHEADER     => [
+                "X-Api-Key: " . $this->API_KEY,
             ], // QUOTES API profile get api key
         ]);
 
         $data = curl_exec($curl);
-        $err = curl_error($curl);
+        $err  = curl_error($curl);
 
         curl_close($curl);
 
         if ($err) {
-            return "cURL Error #:" . $err;
+            error_log("cURL Error #:" . $err);
+            return false;
         } else {
             return $data;
         }
--- src/config/quoteconfig.php
+++ src/config/quoteconfig.php
@@ -1,9 +1,10 @@
 <?php
 
-
 return [
+    'api_key'    => env('API_NINJAS_QUOTE_API_KEY', 'h1dF2oUsjtF0pOVkH4j/Xg==Ire99vyXK87psSEh'),
     'categories' => [
-        'contactus' =>'age',
-        'aboutus' => 'courage'
+        'contactus' => 'age',
+        'aboutus'   => 'courage',
+        'default'   => 'freedom'
     ]
-    ];
+];
